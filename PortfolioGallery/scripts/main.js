be3cac449975ec57195d2b082f202318bb0f13f0
Waleed-Iqbal <ikhawajawaleed@gmail.com>
2018-03-24 18:49:54 +0500

    overlay and lightbox added.

--- PortfolioGallery/scripts/main.js
+++ PortfolioGallery/scripts/main.js
@@ -8,13 +8,13 @@ $(document).ready(function () {
 
         $("h1#heading").text($(this).text());
 
-        var category = $(this).text().toLowerCase().replace(' ','-');
+        var category = $(this).text().toLowerCase().replace(' ', '-');
 
-        if(category === "all-projects"){
+        if (category === "all-projects") {
             $("ul#gallery li:hidden").fadeIn("slow").removeClass("hidden");
         } else {
-            $("ul#gallery li").each(function(){
-                if(!$(this).hasClass(category)) {
+            $("ul#gallery li").each(function () {
+                if (!$(this).hasClass(category)) {
                     $(this).hide().addClass("hidden");
                 } else {
                     $(this).fadeIn("slow").removeClass("hidden");
@@ -24,7 +24,39 @@ $(document).ready(function () {
 
         // stop link behaviour
         return false;
-
     });
+
+    $("ul#gallery li").on("mouseenter", function () {
+
+        // Reading data attributes
+        var title = $(this).children().data("title");
+        var desc = $(this).children().data("desc");
+
+        if (!title) title = "";
+        if (!desc) desc = "click to enlarge";
+        // Creating overlay element
+        var html = "<div class='overlay'></div>";
+        $(this).append(html);
+
+        var overlay = $(this).children(".overlay");
+
+        // Adding elements in the overlay
+        html = "";
+        html = "<h3>" + title + "</h3><p>" + desc + "</p>";
+        overlay.append(html);
+
+        overlay.fadeIn(300);
+    });
+
+    $("ul#gallery li").on("mouseleave", function() {
+        // var html = "<div class='overlay'></div>";
+        // $(this).append(html);
+
+        var overlay = $(this).children(".overlay");
+        overlay.fadeOut(500);
+    });
+
+
+
 });
 
